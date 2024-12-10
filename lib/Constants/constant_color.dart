2e76bba6d250b2195ce5737a8275f3415a28536c
Mariam Nasr Eldin <11410120211092@stud.cu.edu.eg>
2024-12-10 16:27:53 +0200

    edits

--- lib/Constants/constant_color.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:ui';
-
-Color mainColor = const Color.fromARGB(255, 255, 252, 245);//
-Color videoColor = const Color.fromARGB(255,236, 223, 208);
-Color buttonsLabel = const Color.fromARGB(255, 255, 252, 243);//
-Color orange = const Color.fromARGB(255, 236, 187, 104);//
-Color darkOlive = const  Color.fromARGB(255, 85, 76, 42);//
--- lib/features/Authentication/data/userModel.dart
+++
@@ -1,6 +0,0 @@
-class userModel{
-  final String name;
-  final String email;
-  final String password;
-  userModel({required this.name,required this.email,required this.password});
-}
