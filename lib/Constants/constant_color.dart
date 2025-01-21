a1e8e2a29b313ed4875adbefbd9b91df2b38ae59
Mariam Nasr Eldin <101672548+marimNasr@users.noreply.github.com>
2025-01-21 22:40:47 +0200

    Delete Constants directory

--- lib/Constants/constant_color.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-Color mainColor = const Color.fromARGB(255, 255, 252, 245);//
-Color selectedCategory = const Color.fromARGB(255, 255, 249, 234);
-Color videoColor = const Color.fromARGB(255,236, 223, 208);
-Color buttonsLabel = const Color.fromARGB(255, 255, 252, 243);//
-Color orange = const Color.fromARGB(255, 236, 187, 104);//
-Color darkOlive = const  Color.fromARGB(255, 85, 76, 42);//
